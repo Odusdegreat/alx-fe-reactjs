798aabc5214ae6f57797627245fce551e2a8697c
odusdegreat <tobiodubote@gmail.com>
2025-02-17 18:57:36 +0100

    initial commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,17 @@
 import UserProfile from "./components/UserProfile";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import WelcomeMessage from "./components/WelcomeMessage";
+import MainContent from "./components/MainContent";
 
 function App() {
   return (
     <div>
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+      <UserProfile />
+      <Footer />
       <h1>Welcome to My React App</h1>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
