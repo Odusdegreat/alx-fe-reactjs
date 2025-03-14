8c6932b37c5be515f6c93fe5e94a4be8ea13b4f0
odusdegreat <tobiodubote@gmail.com>
2025-03-14 15:56:39 +0100

    initial commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,15 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center sm:p-4 md:p-8 sm:max-w-xs md:max-w-xs">
+    <div className="bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className=" rounded-full mx-auto sm:w-24  md:w-36"
+        className="w-24 sm:w-24 md:w-36 h-24 sm:h-24 md:h-36 rounded-full mx-auto"
       />
-      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">
+      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">
         John Doe
       </h1>
-      <p className="text-gray-600 text-base sm:text-sm md:text-base">
+      <p className="text-sm sm:text-base md:text-lg text-gray-600">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
