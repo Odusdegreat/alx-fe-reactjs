518929b7ddc26304f7990422ad0004c798abb2dd
odusdegreat <tobiodubote@gmail.com>
2025-03-30 23:32:14 +0100

    initial commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const GITHUB_API_BASE_URL = "https://api.github.com/search/users";
+const GITHUB_API_BASE_URL = "https://api.github.com/search/users?q=";
 
 export const fetchUsers = async (username, location, minRepos) => {
   try {
-    let query = `q=${username}`;
+    let query = `${username}`;
 
     if (location) {
       query += `+location:${location}`;
@@ -14,7 +14,7 @@ export const fetchUsers = async (username, location, minRepos) => {
     }
 
     const response = await axios.get(
-      `${GITHUB_API_BASE_URL}?${query}&per_page=10`
+      `${GITHUB_API_BASE_URL}${query}&per_page=10`
     );
     return response.data.items; // Extract user list
   } catch (error) {
